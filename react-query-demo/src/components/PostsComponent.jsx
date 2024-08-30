4522c017f2ce4137e8e41ebe94793c36f83b2ebc
Edom Feleke <edinafeleke@gmail.com>
2024-08-30 22:47:12 +0300

    commit chnage

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,32 +1,33 @@
 // src/components/PostsComponent.jsx
 import React from 'react';
 import { useQuery } from 'react-query';
+import axios from 'axios';
 
-// Function to fetch posts from the API
 const fetchPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
 };
 
 function PostsComponent() {
-  // Using useQuery to fetch data
-  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  const { data, error, isLoading, isError, isFetching } = useQuery(
+    'posts', // Query key
+    fetchPosts, // Fetch function
+    {
+      cacheTime: 1000 * 60 * 10, // Cache data for 10 minutes
+      staleTime: 1000 * 60 * 5,  // Data is fresh for 5 minutes
+      refetchOnWindowFocus: true, // Refetch data when the window regains focus
+      keepPreviousData: true, // Keep previous data while fetching new data
+    }
+  );
 
-  // Display loading message while data is being fetched
   if (isLoading) return <div>Loading...</div>;
-
-  // Display error message if there is an error
-  if (isError) return <div>An error occurred: {error.message}</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div>
-      {/* Button to refetch data */}
-      <button onClick={() => refetch()}>Refetch Posts</button>
+      <h2>Posts</h2>
+      {isFetching && <span>Refreshing...</span>}
       <ul>
-        {/* Render list of posts */}
         {data.map(post => (
           <li key={post.id}>{post.title}</li>
         ))}
