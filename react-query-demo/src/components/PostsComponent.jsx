0a850cd9934537688a0fba14d61235c9d3eefb25
Edom Feleke <edinafeleke@gmail.com>
2024-08-30 22:44:47 +0300

    commit chnages

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,7 +1,8 @@
-// src/PostsComponent.jsx
+// src/components/PostsComponent.jsx
 import React from 'react';
 import { useQuery } from 'react-query';
 
+// Function to fetch posts from the API
 const fetchPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   if (!response.ok) {
@@ -11,15 +12,21 @@ const fetchPosts = async () => {
 };
 
 function PostsComponent() {
-  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+  // Using useQuery to fetch data
+  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
 
+  // Display loading message while data is being fetched
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>An error occurred: {error.message}</div>;
+
+  // Display error message if there is an error
+  if (isError) return <div>An error occurred: {error.message}</div>;
 
   return (
     <div>
+      {/* Button to refetch data */}
       <button onClick={() => refetch()}>Refetch Posts</button>
       <ul>
+        {/* Render list of posts */}
         {data.map(post => (
           <li key={post.id}>{post.title}</li>
         ))}
