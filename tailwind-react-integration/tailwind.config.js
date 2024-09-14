416180e84adadf2be048ea8886fff2ed06259c00
Edom Feleke <edinafeleke@gmail.com>
2024-09-14 20:17:17 +0300

    commit change

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  content: [
-    './public/index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+}
