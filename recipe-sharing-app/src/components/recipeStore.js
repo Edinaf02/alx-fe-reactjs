c42db12abd81161ff7049ab3fe770b86d43607c3
Edom Feleke <edinafeleke@gmail.com>
2024-09-15 18:40:30 +0300

    commit change

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,20 @@
-import { create } from 'zustand';
+// src/recipeStore.js
+import create from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({
-    recipes: [...state.recipes, newRecipe]
+
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe],
   })),
-  setRecipes: (recipes) => set({ recipes }),
+
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
 }));
