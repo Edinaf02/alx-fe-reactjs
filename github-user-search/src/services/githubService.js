574518189ab40b23c928d1ef28ca1753e78e0e8e
Edom Feleke <edinafeleke@gmail.com>
2024-09-19 15:12:51 +0300

    update changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://api.github.com/search/users';
-
 export const fetchUserData = async ({ username, location, repos }) => {
-  let query = `q=${username}`;
-  if (location) query += `+location:${location}`;
-  if (repos) query += `+repos:>${repos}`;
+  const query = `q=${username}+location:${location}+repos:>${repos}`;
+  const response = await fetch(`https://api.github.com/search/users?${query}`);
   
-  const response = await axios.get(`${BASE_URL}?${query}`);
-  return response.data.items;
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  return data.items;
 };
