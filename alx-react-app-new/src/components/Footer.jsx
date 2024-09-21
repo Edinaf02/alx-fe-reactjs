3d88bd5decc677c35e42974f5e43124f49cc1f75
Edom Feleke <edinafeleke@gmail.com>
2024-09-21 18:43:46 +0300

    commit changes

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: '#fff', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2024 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'left' }}>
+      <p>Here is some main content about my favorite cities!</p>
+    </main>
+  );
+}
+
+export default MainContent;
