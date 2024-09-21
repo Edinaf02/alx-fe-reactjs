11957e5556abb4e9b5d377afb7459520fe1957be
Edom Feleke <edinafeleke@gmail.com>
2024-09-21 19:13:29 +0300

    update commit

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react';
-import UserContext from './UserContext';
+import UserContext from '../UserContext';
 
 function UserDetails() {
-  // Access user data from context
   const userData = useContext(UserContext);
 
   return (
