f1a058795077a0c269184bc1c2fe749492c18645
Edom Feleke <edinafeleke@gmail.com>
2024-09-21 18:47:57 +0300

    update  src/components/MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'left' }}>
-      <p>Here is some main content about my favorite cities!</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
