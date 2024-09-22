4ecca1acaecc3bc2985b7447174e1d1e397071a9
Edom Feleke <edinafeleke@gmail.com>
2024-09-22 20:56:02 +0300

    commit change

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage'; // Import the WelcomeMessage component
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage /> {/* Include the WelcomeMessage component */}
+        </div>
+    );
+}
+
+export default App;
